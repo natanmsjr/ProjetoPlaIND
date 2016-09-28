2126e8c57ba09066ce1131c2121f64d6931d1d5c
Natanael <natan.msjr@gmail.com>
2016-09-28 12:33:31 -0300

    Atualização 28/09/2016  - Novas atualizações na página principal

---
+++ modules/public/views/home/home3.php
@@ -0,0 +1,180 @@
+    <!-- Header Carousel de Imagens -->
+    <header id="myCarousel" class="carousel slide">
+        
+        <!-- Indicadores -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+        </ol>
+
+        <!-- Itens (Imagem) dos slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <div class="fill" style="background-image:url(../assets/images/myCarousel/bg1.jpg);"></div>
+                <div class="carousel-caption">
+                    <!-- <h3>Legenda</h3>
+                    <a class="btn btn-default" href="#">Read More</a> -->
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url(../assets/images/myCarousel/bg2.jpg);"></div>
+                <div class="carousel-caption">
+                    <!-- <h3>Legenda</h3>
+                    <a class="btn btn-default" href="#">Read More</a> -->
+                </div>
+            </div>
+        </div>
+
+        <!-- Controles -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+        
+        <div style="background-color:#777; height:4px;"></div>
+        
+    </header>
+    <!-- /.carousel slide> -->
+    
+    <div class="container">
+        
+        <!-- Conheça a Plataforma -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h3 class="page-header">Conheça a Plataforma</h3>
+                
+                <div class="text-center">
+                    <h4>Acompanhe o andamento do seu pedido de proteção de um modo muito mais simples, rápido e eficiente</h4>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+        
+        <!-- Painel de Serviços -->
+        <!-- The circle icons use Font Awesome's stacked icon classes. For more information, visit http://fontawesome.io/examples/ -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Propriedade Industrial</h2>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="panel panel-default text-center">
+                    <div class="panel-heading">
+                        <span class="fa-stack fa-5x">
+                              <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                              <i class="fa fa-file-image-o fa-stack-1x fa-inverse"></i>
+                        </span>
+                    </div>
+                    <div class="panel-body">
+                        <h4>Desenho Industrial</h4>
+                        <p>Acompanhe seu processo de proteção de Desenho Industrial</p>
+                        <a href="/propriedade_industrial/desenho_industrial" class="btn btn-primary">Saiba mais</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="panel panel-default text-center">
+                    <div class="panel-heading">
+                        <span class="fa-stack fa-5x">
+                              <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                              <i class="fa fa-file-excel-o fa-stack-1x fa-inverse"></i>
+                        </span>
+                    </div>
+                    <div class="panel-body">
+                        <h4>Marca</h4>
+                        <p>Acompanhe seu processo de registro de Marca</p>
+                        <a href="/propriedade_industrial/marca" class="btn btn-primary">Saiba mais</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="panel panel-default text-center">
+                    <div class="panel-heading">
+                        <span class="fa-stack fa-5x">
+                              <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                              <i class="fa fa-file-powerpoint-o fa-stack-1x fa-inverse"></i>
+                        </span>
+                    </div>
+                    <div class="panel-body">
+                        <h4>Patente</h4>
+                        <p>Acompanhe seu processo de proteção de Patente</p>
+                        <a href="/propriedade_industrial/patente" class="btn btn-primary">Saiba mais</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="panel panel-default text-center">
+                    <div class="panel-heading">
+                        <span class="fa-stack fa-5x">
+                              <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                              <i class="fa fa-file-code-o fa-stack-1x fa-inverse"></i>
+                        </span>
+                    </div>
+                    <div class="panel-body">
+                        <h4>Software</h4>
+                        <p>Acompanhe seu processo de resgistro de Software</p>
+                        <a href="/propriedade_industrial/software" class="btn btn-primary">Saiba mais</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Dispositivos -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Suporte a diversos dispositivos</h2>
+                <div class="col-lg-6">
+                    <p>Queremos que você tenha a melhor experiência. Acompanhe em qualquer lugar.</p>
+                </div>
+            <div class="col-md-6">
+                <img class="img-responsive" src="../assets/images/freatures/responsive.png" alt="">
+            </div>
+        </div>
+        
+        <!-- /.row -->
+		
+        <!-- Instituição -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Instituição</h2>
+            
+                <div class="row">
+                    <!-- Map Column -->
+                    <div class="col-md-8">
+                        <!-- Embedded Google Map -->
+                        <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d62678.645469952295!2d-37.09312987870823!3d-10.931965232702705!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x71ab2423841cf53%3A0xade52498616df289!2sUniversidade+Federal+de+Sergipe!5e0!3m2!1spt-BR!2sbr!4v1472482531190" width="600" height="450" frameborder="0" style="border:0" allowfullscreen"></iframe>
+                    </div>
+                    <!-- Contact Details Column -->
+                    <div class="col-md-4">
+                        <h3><strong>CINTTEC</strong><br><small>Universidade Federal de Sergipe</small></h3>
+                        <p>
+                            Av. Marechal Rondon, S/N<br>Jardim Rosa Elze, São Cristóvão 49100-000<br>
+                        </p>
+                        <p><i class="fa fa-phone"></i> 
+                            <abbr title="Phone"></abbr>: (79) 3194-6865</p>
+                        <p><i class="fa fa-envelope-o"></i> 
+                            <abbr title="Email"></abbr>: <a href="mailto:name@example.com">cinttec.ufs@gmail.com</a>
+                        </p>
+                        <p><i class="fa fa-clock-o"></i> 
+                            <abbr title="Hours"></abbr>: Segunda - Sexta: 8:00h ás 17:00 </p>
+                        <ul class="list-unstyled list-inline list-social-icons">
+                            <li>
+                                <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-linkedin-square fa-2x"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-twitter-square fa-2x"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-google-plus-square fa-2x"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </div>
